d3224d2e27c57fe534781cf3ffaf378ec72b5444
Kellie Au <kjad2022@mymail.pomona.edu>
2025-02-28 20:14:08 -0800

    working on header + added results page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,19 @@
 import RedirectButton from '@/components/RedirectButton'
+import PickupHeader from '@/components/PickupHeader'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-100 text-black">
-      <h1 className="mb-4 text-3xl font-bold">Welcome to the Home Page</h1>
-      <p className="mb-6">This is the Home page.</p>
+    <div className="flex min-h-screen w-full flex-col bg-gray-100 text-black">
+      {/* Header at the top */}
+      <PickupHeader />
 
-      {/* Reusable Redirect Button */}
-      <RedirectButton label="Go to Questionnaire" route="/questionnaire" />
+      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-100 text-black">
+        <h1 className="mb-4 text-3xl font-bold">Welcome to the Home Page</h1>
+        <p className="mb-6">This is the Home page.</p>
+
+        {/* Reusable Redirect Button */}
+        <RedirectButton label="Go to Questionnaire" route="/questionnaire" />
+      </div>
     </div>
   )
 }
