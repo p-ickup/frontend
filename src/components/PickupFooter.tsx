251e0941c19c9e7419c0565ede9b8473d249dce9
GenericName-161 <52934438+GenericName-161@users.noreply.github.com>
2025-04-13 11:20:08 -0700

    Footer Component and some colors

---
+++ src/components/PickupFooter.tsx
@@ -0,0 +1,15 @@
+import SimpleRedirectButton from "./buttons/SimpleRedirectButton";
+
+export default function PickupFooter() {
+    return (
+      <footer className="w-full border-t border-t-foreground/60 p-8 bg-gradient-to-r from-teal-500 to-yellow-100 text-white mt-8">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+          <div className="flex space-x-4">
+            <SimpleRedirectButton label="About" route="/about" />
+            <SimpleRedirectButton label="Contact" route="/contact" />
+            <SimpleRedirectButton label="FAQ" route="/faq" />
+          </div>
+        </div>
+      </footer>
+    );
+  }
