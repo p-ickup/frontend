d8a743112227126d1d63a25956e3e5f43d09c52e
GenericName-161 <52934438+GenericName-161@users.noreply.github.com>
2025-04-20 15:42:44 -0700

    Material Tailwind install and removing dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   darkMode: ['class'],
   content: ['./src/**/*.{ts,tsx}'],
   prefix: '',
@@ -67,4 +69,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
+});
