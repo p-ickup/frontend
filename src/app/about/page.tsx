d83cbd3ff50504d3a1010b91b896818ac38cd902
GenericName-161 <52934438+GenericName-161@users.noreply.github.com>
2025-04-19 23:47:24 -0700

    Added about, contact, and faq page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,52 @@
+export default function About() {
+    return(
+        <div className="min-h-[calc(100vh-165px)] bg-gray-100 text-black p-16">
+            <h1 className="text-4xl mb-8">
+                About P-ickup
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eos praesentium alias corrupti asperiores illum sunt neque voluptatum minus reiciendis aut numquam sed porro nihil error, labore veritatis, nisi ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi similique repudiandae ducimus vel. Ad, minima corrupti asperiores optio non similique accusantium tempore consequuntur quisquam quo cupiditate aspernatur est a eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quaerat! Maxime harum fuga, consequuntur, officia, dicta voluptatum aliquam neque eaque non necessitatibus nam a vitae tempora aperiam distinctio magnam autem.
+            </p>
+            <h1 className="text-4xl mb-8 mt-8">
+                How it Works
+            </h1>
+            <div className="border-l-2 border-gray-400 ml-1">
+                <div className="mb-8 flex items-center">
+                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">1</div>
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Step 1</h2>
+                        <p className="text-gray-600">Lorem ipsum ecessitatibus voluptate distinctio vitae molestias animi veritatis similique eos cupiditate, magni, dolore temporibus autem repellat quos ipsum fugiat.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">2</div>
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Step 2</h2>
+                        <p className="text-gray-600">Lorem ipiti autem laborum illo exercitationem molestiae numquam vel aspernatur ab quam, reiciendis reprehenderit.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">3</div>
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Step 3</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet conr deleniti. Et modi, explicabo dolor a laborum eaque quam minus non eius illum, animi harum fugit repellendus architecto.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">4</div>
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Step 4</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet conil, esse numquam labore ex?aborum numquam?</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">5</div>
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Step 5</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quos est commodi aliquid, in aliqu!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/faq/page.tsx
@@ -0,0 +1,66 @@
+export default function Faq() {
+    return(
+        <div className="min-h-[calc(100vh-165px)] text-black p-20 bg-gradient-to-r from-teal-200 to-yellow-100">
+            <h1 className="text-5xl mb-16 font-semibold flex justify-center">
+                Frequently Asked Questions
+            </h1>
+            <p className="flex m-8">Small blurb about P-ickup Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita nobis distinctio maxime saepe quas dolor assumenda velit ab qui ipsum nulla nemo, architecto dolore dignissimos ducimus est eaque culpa? Rem.</p>
+            <div className="flex justify-center flex-col items-center">
+                <div className="mb-8">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Can I edit my questionnaire?</h2>
+                        <p className="text-gray-600">You can edit your information up to <strong>3 days before your flight.</strong> Otherwise, you must cancel it and send in a new questionnaire. In that case, on your questionnaire, you can opt in to be placed in the unmatched section where you can try to find others who you may be able to split a ride with.</p>
+                    </div>
+                </div>
+                <div className="mb-8">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">When will I know who I am matched with?</h2>
+                        <p className="text-gray-600">We will display your match 3 days before your flight, unless you do not submit your questionnaire more than 3 days in advance. In that case, on your questionnaire, you can opt in to be placed in the unmatched section where you can try to find others who you may be able to split a ride with.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">What happens if I need to cancel?</h2>
+                        <p className="text-gray-600">endis reprehenderit.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">How exactly does P-ickup match me?</h2>
+                        <p className="text-gray-600">s non eius illum, animi harum fugit repellendus architecto.</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">What information is shared with other users?</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sitnumquam?</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">What happens if P-ickup does not find me a match?</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectequ!</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">What happens if someone is my group cancels or is delayed?</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectequ!</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">What makes P-ickup special?</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectequ!</p>
+                    </div>
+                </div>
+                <div className="mb-8 flex items-center">
+                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
+                        <h2 className="text-xl font-semibold">Can I get a rematch?</h2>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectequ!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
