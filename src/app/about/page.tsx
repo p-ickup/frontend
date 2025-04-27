987726c184871d549fbf8d6b26190ec677312bf9
GenericName-161 <52934438+GenericName-161@users.noreply.github.com>
2025-04-27 00:41:00 -0700

    Added horizontal timeline

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,52 +1,129 @@
+'use client'
+import React from 'react'
+import { Stepper, Step, Button, Typography } from '@material-tailwind/react'
+
 export default function About() {
-    return(
-        <div className="min-h-[calc(100vh-165px)] bg-gray-100 text-black p-16">
-            <h1 className="text-4xl mb-8">
-                About P-ickup
-            </h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eos praesentium alias corrupti asperiores illum sunt neque voluptatum minus reiciendis aut numquam sed porro nihil error, labore veritatis, nisi ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi similique repudiandae ducimus vel. Ad, minima corrupti asperiores optio non similique accusantium tempore consequuntur quisquam quo cupiditate aspernatur est a eveniet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quaerat! Maxime harum fuga, consequuntur, officia, dicta voluptatum aliquam neque eaque non necessitatibus nam a vitae tempora aperiam distinctio magnam autem.
-            </p>
-            <h1 className="text-4xl mb-8 mt-8">
-                How it Works
-            </h1>
-            <div className="border-l-2 border-gray-400 ml-1">
-                <div className="mb-8 flex items-center">
-                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">1</div>
-                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
-                        <h2 className="text-xl font-semibold">Step 1</h2>
-                        <p className="text-gray-600">Lorem ipsum ecessitatibus voluptate distinctio vitae molestias animi veritatis similique eos cupiditate, magni, dolore temporibus autem repellat quos ipsum fugiat.</p>
-                    </div>
-                </div>
-                <div className="mb-8 flex items-center">
-                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">2</div>
-                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
-                        <h2 className="text-xl font-semibold">Step 2</h2>
-                        <p className="text-gray-600">Lorem ipiti autem laborum illo exercitationem molestiae numquam vel aspernatur ab quam, reiciendis reprehenderit.</p>
-                    </div>
-                </div>
-                <div className="mb-8 flex items-center">
-                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">3</div>
-                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
-                        <h2 className="text-xl font-semibold">Step 3</h2>
-                        <p className="text-gray-600">Lorem ipsum dolor sit amet conr deleniti. Et modi, explicabo dolor a laborum eaque quam minus non eius illum, animi harum fugit repellendus architecto.</p>
-                    </div>
-                </div>
-                <div className="mb-8 flex items-center">
-                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">4</div>
-                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
-                        <h2 className="text-xl font-semibold">Step 4</h2>
-                        <p className="text-gray-600">Lorem ipsum dolor sit amet conil, esse numquam labore ex?aborum numquam?</p>
-                    </div>
-                </div>
-                <div className="mb-8 flex items-center">
-                    <div className="w-8 h-8 bg-teal-500 rounded-full flex items-center justify-center text-white font-bold ml-1">5</div>
-                    <div className="ml-4 p-4 bg-white rounded-lg shadow-md hover:bg-slate-50">
-                        <h2 className="text-xl font-semibold">Step 5</h2>
-                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quos est commodi aliquid, in aliqu!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  const [activeStep, setActiveStep] = React.useState(0)
+  const [isLastStep, setIsLastStep] = React.useState(false)
+  const [isFirstStep, setIsFirstStep] = React.useState(false)
+
+  const handleNext = () => !isLastStep && setActiveStep((cur) => cur + 1)
+  const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1)
+
+  return (
+    <div className="min-h-[calc(100vh-165px)] bg-gray-100 p-16 text-black">
+      <h1 className="mb-8 text-4xl">About P-ickup</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eos
+        praesentium alias corrupti asperiores illum sunt neque voluptatum minus
+        reiciendis aut numquam sed porro nihil error, labore veritatis, nisi ea.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        similique repudiandae ducimus vel. Ad, minima corrupti asperiores optio
+        non similique accusantium tempore consequuntur quisquam quo cupiditate
+        aspernatur est a eveniet. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Doloribus, quaerat! Maxime harum fuga, consequuntur,
+        officia, dicta voluptatum aliquam neque eaque non necessitatibus nam a
+        vitae tempora aperiam distinctio magnam autem.
+      </p>
+      <h1 className="mt-8 text-4xl">How it Works</h1>
+
+      <Stepper
+        activeStep={activeStep}
+        isLastStep={(value) => setIsLastStep(value)}
+        isFirstStep={(value) => setIsFirstStep(value)}
+        className="mr-10 p-20"
+        lineClassName="bg-teal-500/50"
+        activeLineClassName="bg-teal-500"
+      >
+        <Step
+          onClick={() => setActiveStep(0)}
+          activeClassName="!bg-teal-500"
+          completedClassName="!bg-teal-300"
+          className="!bg-teal-300 text-white"
+        >
+          1
+          <div className="absolute -bottom-[4.5rem] w-max text-center">
+            <Typography
+              variant="h6"
+              color={activeStep === 0 ? 'blue-gray' : 'gray'}
+            >
+              Months in Advance
+            </Typography>
+            <Typography
+              color={activeStep === 0 ? 'blue-gray' : 'gray'}
+              className="font-normal"
+            >
+              Book your flight.
+            </Typography>
+          </div>
+        </Step>
+        <Step
+          onClick={() => setActiveStep(1)}
+          activeClassName="!bg-teal-500"
+          completedClassName="!bg-teal-300"
+          className="!bg-teal-300 text-white"
+        >
+          2
+          <div className="absolute -bottom-[4.5rem] w-max text-center">
+            <Typography
+              variant="h6"
+              color={activeStep === 1 ? 'blue-gray' : 'gray'}
+            >
+              7 Days
+            </Typography>
+            <Typography
+              color={activeStep === 1 ? 'blue-gray' : 'gray'}
+              className="font-normal"
+            >
+              Complete P-ickup questionnaire.
+            </Typography>
+          </div>
+        </Step>
+        <Step
+          onClick={() => setActiveStep(2)}
+          activeClassName="!bg-teal-500"
+          completedClassName="!bg-teal-300"
+          className="!bg-teal-300 text-white"
+        >
+          3
+          <div className="absolute -bottom-[4.5rem] w-max text-center">
+            <Typography
+              variant="h6"
+              color={activeStep === 2 ? 'blue-gray' : 'gray'}
+            >
+              3 Days
+            </Typography>
+            <Typography
+              color={activeStep === 2 ? 'blue-gray' : 'gray'}
+              className="font-normal"
+            >
+              Recieve your match and contact.
+            </Typography>
+          </div>
+        </Step>
+        <Step
+          onClick={() => setActiveStep(3)}
+          activeClassName="!bg-teal-500"
+          completedClassName="!bg-teal-300"
+          className="!bg-teal-300 text-white"
+        >
+          4
+          <div className="absolute -bottom-[4.5rem] w-max text-center">
+            <Typography
+              variant="h6"
+              color={activeStep === 2 ? 'blue-gray' : 'gray'}
+            >
+              0 Days!
+            </Typography>
+            <Typography
+              color={activeStep === 2 ? 'blue-gray' : 'gray'}
+              className="font-normal"
+            >
+              Get P-icked up! Fill out feedback.
+            </Typography>
+          </div>
+        </Step>
+      </Stepper>
+    </div>
+  )
 }
