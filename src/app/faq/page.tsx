15aab19c8c7ac3101eded3f84072b49705454769
GenericName-161 <52934438+GenericName-161@users.noreply.github.com>
2025-04-30 23:08:26 -0700

    Improvements to faq page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,6 +6,7 @@ import {
   AccordionBody,
 } from '@material-tailwind/react'
 import SimpleRedirectButton from '@/components/buttons/SimpleRedirectButton'
+import RedirectButton from '@/components/buttons/RedirectButton'
 
 export default function Faq() {
   const [open, setOpen] = React.useState(1)
@@ -18,34 +19,39 @@ export default function Faq() {
       <h1 className="mb-16 flex justify-center text-5xl font-semibold">
         Frequently Asked Questions
       </h1>
-      <p className="m-8 flex justify-center">
-        No one wants to spend $100 on a single Uber ride. We are here to change
-        that.
+      <p className="mb-8 flex justify-center">
+        No one wants to spend $100 on a single Uber ride. We&apos;re here to
+        change that.
       </p>
       <Accordion
         open={open === 1}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(1)}>
+        <AccordionHeader
+          onClick={() => handleOpen(1)}
+          className="flex justify-center"
+        >
           Can I edit my questionnaire?
         </AccordionHeader>
-        <AccordionBody>
-          You can edit your information{' '}
-          <strong>UP TO 3 DAYS BEFORE YOUR FLIGHT.</strong> Otherwise, you must
-          cancel it and send in a new questionnaire. In that case, on your
-          questionnaire, you can opt in to be placed in the unmatched section
-          where you can try to find others who you may be able to split a ride
-          with.
+        <AccordionBody className="text-md flex justify-center text-center text-black">
+          You can edit your information UP TO 3 DAYS BEFORE YOUR FLIGHT.
+          Otherwise, you must cancel it and send in a new questionnaire. In that
+          case, on your questionnaire, you can opt in to be placed in the
+          unmatched section where you can try to find others who you may be able
+          to split a ride with.
         </AccordionBody>
       </Accordion>
       <Accordion
         open={open === 2}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(2)}>
+        <AccordionHeader
+          onClick={() => handleOpen(2)}
+          className="flex justify-center"
+        >
           When will I know who I am matched with?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           We will display your match 3 DAYS BEFORE YOUR FLIGHT, unless you do
           not submit your questionnaire more than 3 days in advance. In that
           case, on your questionnaire, you can opt in to be placed in the
@@ -55,12 +61,15 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 3}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(3)}>
+        <AccordionHeader
+          onClick={() => handleOpen(3)}
+          className="flex justify-center"
+        >
           What happens if I’m not matched?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           When you fill out your questionnaire form, you have the option to opt
           in to be placed in the unmatched section if we can’t find you a match.
           This page will help you find others who you may be able to split a
@@ -69,13 +78,16 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 4}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(4)}>
+        <AccordionHeader
+          onClick={() => handleOpen(4)}
+          className="flex justify-center"
+        >
           What happens if I or someone in my group has a delayed/canceled
           flight?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           Please utilize the commenting system on your match so that you can
           update your group members about any issues. We hope your group can
           come to an agreement if this happens, for example, how long of a delay
@@ -85,13 +97,16 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 5}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(5)}>
+        <AccordionHeader
+          onClick={() => handleOpen(5)}
+          className="flex justify-center"
+        >
           What if I need to cancel my questionnaire/match due to unforeseen
           circumstances?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           We understand travelling can be unpredictable. If at all possible,
           please cancel your questionnaire form for your flight at least 3 days
           in advance of your flight. This way, you will not be affecting any
@@ -102,12 +117,15 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 6}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(6)}>
+        <AccordionHeader
+          onClick={() => handleOpen(6)}
+          className="flex justify-center"
+        >
           Can I get a rematch?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           You cannot request a rematch at this time. If you are dissatisfied
           with your match, you can cancel. If you resend in a questionnaire and
           opt in to being in the unmatched section, you can find others who you
@@ -116,28 +134,35 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 7}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(7)}>
+        <AccordionHeader
+          onClick={() => handleOpen(7)}
+          className="flex justify-center"
+        >
           How does the Unmatched page work?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           If you are within 3 days of your upcoming flight and have not been
           matched, you can check out our unmatched page and try to join other
           groups! Browse through groups and individuals who are in the same boat
-          as you. Send a join request if you find something that works for you!
+          as you. Send a join request if you find something that works for you!{' '}
+          <br></br> <br></br>
           NOTE: this is only available if you opted in when you filled out your
           questionnaire for that flight.
         </AccordionBody>
       </Accordion>
       <Accordion
         open={open === 8}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(8)}>
+        <AccordionHeader
+          onClick={() => handleOpen(8)}
+          className="flex justify-center"
+        >
           How exactly am I being matched?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           We are using a machine learning model to group all of our existing
           match requests by flight time, bag numbers, terminals, drop off
           points, and other important details. Our model’s performance improves
@@ -148,12 +173,15 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 9}
-        className="mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(9)}>
+        <AccordionHeader
+          onClick={() => handleOpen(9)}
+          className="flex justify-center"
+        >
           What information is shared with other users?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           We aim to keep our users as anonymous as possible. Once you’ve matched
           with a group, we provide the group members your first name and email.
           We do not share your flight info, last name, or phone number. Further,
@@ -162,12 +190,15 @@ export default function Faq() {
       </Accordion>
       <Accordion
         open={open === 10}
-        className="mb-12 rounded-lg bg-gray-100 p-5 shadow-md"
+        className="hover:bg-gray-150 mb-8 rounded-lg bg-gray-100 p-5 shadow-md"
       >
-        <AccordionHeader onClick={() => handleOpen(10)}>
+        <AccordionHeader
+          onClick={() => handleOpen(10)}
+          className="flex justify-center"
+        >
           What if I didn’t get the price that I was hoping for?
         </AccordionHeader>
-        <AccordionBody>
+        <AccordionBody className="text-md flex justify-center text-center text-black">
           We will try our best to get a match that has a price within your
           budget but we cannot guarantee this. We assure you that our machine
           learning model will try to find the optimal match for you! Our model’s
@@ -176,9 +207,9 @@ export default function Faq() {
           be better!
         </AccordionBody>
       </Accordion>
-      <h2 className="text-lg font-semibold">
-        If you have any other questions, please{' '}
-        <SimpleRedirectButton label="Contact Us." route="/contact" />
+      <h2 className="flex flex-col items-center justify-center text-lg font-semibold">
+        If you have any other questions,
+        <RedirectButton label="Contact Us" route="/contact" />
       </h2>
     </div>
   )
