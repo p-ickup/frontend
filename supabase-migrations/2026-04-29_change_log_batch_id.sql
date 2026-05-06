6b17607b624497f4c29ef3ec15e662e4a0a3b650
framor81 <francisco.moralespuente2022@gmail.com>
2026-05-06 12:27:54 -0700

    Remove migration file from tracked changes

--- supabase-migrations/2026-04-29_change_log_batch_id.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-alter table public."ChangeLog"
-add column if not exists change_batch_id uuid;
-
-create index if not exists idx_changelog_change_batch_id
-on public."ChangeLog" (change_batch_id);
-
-commit;
